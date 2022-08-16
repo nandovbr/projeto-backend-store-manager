e9fe315b3aeb150e60cf7e239c1475e34df59b98
nandovbr <nandovbr15@hotmail.com>
2022-08-16 20:44:07 -0300

    adiciona desafio 2

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -8,7 +8,7 @@ describe("Teste controller products", () => {
 
   describe("Listagem de produtos é retornado pela API", () => {
     
-    it('Deve retornar todos os produtos na controller', async () => {
+    it('Deve retornar o erro de todos os produtos na controller', async () => {
       sinon.stub(productServices, 'getAllProducts').resolves({ error: { code: 404, message: 'Product not found' } });
       const req = {};
       const res = {};
@@ -19,25 +19,26 @@ describe("Teste controller products", () => {
       // res.end = sinon.stub().returns();
       next = sinon.stub().returns();
       
-      await productController.getAllProducts(req, res);
+      await productController.getAllProducts(req, res, next);
       expect(res.status.calledWith(404)).to.be.true;
-      expect(res.status.calledWith({ message: 'Product not found' })).to.be.true;
+      expect(res.json.calledWith({ message: 'Product not found' })).to.be.true;
     });
 
-    it('Deve retornar os produtos na controller pelo id', async () => {
+    it('Deve retornar o erro dos produtos na controller pelo id', async () => {
       sinon.stub(productServices, 'getProductsId').resolves({ error: { code: 404, message: 'Product not found' } });
       const req = {};
       const res = {};
       let next = () => {};
-  
+      
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
       // res.end = sinon.stub().returns();
       next = sinon.stub().returns();
       
-      await productController.getProductsId(req, res);
+      req.params = { id: 1 };
+      await productController.getProductsId(req, res, next);
       expect(res.status.calledWith(404)).to.be.true;
-      expect(res.status.calledWith({ message: 'Product not found' })).to.be.true;
+      expect(res.json.calledWith({ message: 'Product not found' })).to.be.true;
     });
   });
 });
